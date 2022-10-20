f3ea203a3b83884de3aa8d9d8a79dc15483947d6
st2eve <houbart.steeve@gmail.com>
2022-10-20 15:56:15 +0200

    ws

--- src/App.vue
+++ src/App.vue
@@ -1,114 +1,18 @@
 <!-- eslint-disable vue/require-v-for-key -->
 <!-- eslint-disable prettier/prettier -->
 <script>
-export default {
-  data() {
-    return {
-      brand: 'Vue Mastery',
-      cart: 0,
-      product: "socks",
-      description: "Beautiful and soft touch socks",
-      image: "./src/assets/images/socks_blue.jpg",
-      url: "https://vuejs.org/guide/introduction.html",
-      inventory: 15,
-      onSale: true,
-      details: ["50% coton", "30% wool", "20% polyester"],
-      sizes: ["XS", "S", "M", "L", "XL"],
-      variants: [
-        {
-          id: 1,
-          color: "green",
-          image: "./src/assets/images/socks_green.jpg",
-        },
-        {
-          id: 2,
-          color: "blue",
-          image: "./src/assets/images/socks_blue.jpg",
-        },
-      ],
-    };
-  },
-  methods: {
-    addToCart() {
-      if (this.inventory > 0) {
-        this.cart += 1;
-        this.inventory -= 1;
-      }
-    },
-    removeToCart() {
-      if (this.cart > 0) {
-        this.cart -= 1;
-        this.inventory += 1;
-      }
-    },
-    updateImage(variantImage) {
-      this.image = variantImage;
-    },
-  },
-};
-</script>
+import ProductDisplay from './components/ProductDisplay.vue';
 
+export default {
+  components: {
+    ProductDisplay,
+  }
+} 
+
+</script>
 <template>
   <div class="nav-bar"></div>
-  <div class="product-display">
-    <div class="product-container">
-      <div class="product-image">
-        <a :href="url" target="_blank">
-          <img :src="image" :class="{ outOfStockImg: inventory == 0 }" />
-        </a>
-      </div>
-      <div class="product-info">
-        <h1>
-          {{ product }}
-        </h1>
-        <p>
-          {{ description }}
-        </p>
-        <p v-show="onSale">On sale !</p>
-        <p v-if="inventory > 10">In stock</p>
-        <p v-else-if="inventory <= 10 && inventory > 0">Almost sold out !</p>
-        <p v-else>Out of stock</p>
-        <div class="list">
-          <ul>
-            <li v-for="detail in details">
-              {{ detail }}
-            </li>
-          </ul>
-          <ul>
-            <li v-for="size in sizes">
-              {{ size }}
-            </li>
-          </ul>
-        </div>
-        <div class="circle__container">
-          <div
-            v-for="variant in variants"
-            :key="variant.id"
-            @mouseover="updateImage(variant.image)"
-            class="color-circle"
-            :style="{ backgroundColor: variant.color }"
-          ></div>
-        </div>
-        <div class="button__container">
-          <button
-            class="button"
-            @click="addToCart()"
-            :class="{ disabledButton: inventory == 0 }"
-            :disabled="inventory == 0"
-          >
-            Add to cart
-          </button>
-          <button
-            class="button"
-            @click="removeToCart()"
-            :class="{ disabledButton: cart == 0 }"
-            :disabled="cart == 0"
-          >
-            Remove from cart
-          </button>
-        </div>
-      </div>
-      <div class="cart">Cart ({{ cart }})</div>
-    </div>
-  </div>
+  <ProductDisplay />
+  <ProductDisplay />
+  <ProductDisplay />
 </template>
---
+++ src/components/ProductDisplay.vue
@@ -0,0 +1,129 @@
+<script>
+export default {
+    data() {
+        return {
+        brand: 'Vue Mastery',
+        cart: 0,
+        product: "socks",
+        description: "Beautiful and soft touch socks",
+        selectedVariant: 0,
+        url: "https://vuejs.org/guide/introduction.html",
+        onSale: true,
+        details: ["50% coton", "30% wool", "20% polyester"],
+        sizes: ["XS", "S", "M", "L", "XL"],
+        variants: [
+            {
+            id: 1,
+            color: "green",
+            image: "./src/assets/images/socks_green.jpg",
+            quantity: 50,
+            },
+            {
+            id: 2,
+            color: "blue",
+            image: "./src/assets/images/socks_blue.jpg",
+            quantity: 0,
+            },
+        ],
+        };
+    },
+    methods: {
+        addToCart() {
+        if (this.variants[this.selectedVariant].quantity > 0) {
+            this.cart += 1;
+            this.variants[this.selectedVariant].quantity -= 1;
+        }
+        },
+        removeToCart() {
+        if (this.cart > 0) {
+            this.cart -= 1;
+            this.variants[this.selectedVariant].quantity += 1;
+        }
+        },
+        updateVariant(index) {
+        this.selectedVariant = index;
+        },
+    },
+    computed: {
+        title() {
+        return this.brand + ' ' + this.product
+        },
+        image() {
+        return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+        return this.variants[this.selectedVariant].quantity
+        },
+        onSaleDisplay() {
+        if (this.onSale == true){
+            return this.brand + ' ' + this.product + ' is on sale !'
+        }else{
+            return this.brand + ' ' + this.product + ' will be on sale soon !'
+        }
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="product-display">
+        <div class="product-container">
+        <div class="product-image">
+            <a :href="url" target="_blank">
+            <img :src="image" :class="{ outOfStockImg: inventory == 0 }" />
+            </a>
+        </div>
+        <div class="product-info">
+            <h1>
+            {{ title }}
+            </h1>
+            <p>
+            {{ description }}
+            </p>
+            <p>{{ onSaleDisplay }}</p>
+            <p v-if="inStock">In stock</p>
+            <p v-else>Out of stock</p>
+            <div class="list">
+            <ul>
+                <li v-for="detail in details">
+                {{ detail }}
+                </li>
+            </ul>
+            <ul>
+                <li v-for="size in sizes">
+                {{ size }}
+                </li>
+            </ul>
+            </div>
+            <div class="circle__container">
+            <div
+                v-for="(variant, index) in variants"
+                :key="variant.id"
+                @mouseover="updateVariant(index)"
+                class="color-circle"
+                :style="{ backgroundColor: variant.color }"
+            ></div>
+            </div>
+            <div class="button__container">
+            <button
+                class="button"
+                @click="addToCart()"
+                :class="{ disabledButton: !inStock }"
+                :disabled="!inStock"
+            >
+                Add to cart
+            </button>
+            <button
+                class="button"
+                @click="removeToCart()"
+                :class="{ disabledButton: cart == 0 }"
+                :disabled="cart == 0"
+            >
+                Remove from cart
+            </button>
+            </div>
+        </div>
+        <div class="cart">Cart ({{ cart }})</div>
+        </div>
+    </div>
+</template>
